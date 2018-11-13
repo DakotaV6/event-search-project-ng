7e62894acceecfc0be78eb6269ca95647bd46773
Karissa <phillips.karissa00@gmail.com>
2018-11-13 09:44:48 -0500

    opacity added to background and heart icon

--- components/home.js
+++ components/home.js
@@ -3,7 +3,7 @@ const home = {
 
     template:`
     <section class="home">
-    <a href="" ng-click="$ctrl.seeFavs()"> See Your Favorites <3 </a>
+    <a href="" ng-click="$ctrl.seeFavs()"> See Your Favorites <i class="fas fa-heart"></i> </a>
     <h1 class="bold">Flashing Lights Concert Finder</h1>
     <h2 class="bold">Experiences Start Here</h2>
     <button class="start-btn" ng-click="$ctrl.goSearch()">Start</button>
