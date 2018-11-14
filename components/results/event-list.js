54264bc43aac8d7d0c5223c4634eb04c922ba90d
Dakota Vradenburg <41351006+DakotaV6@users.noreply.github.com>
2018-11-14 09:47:01 -0500

    Added container for eventList and started card styling. (#8)

--- components/results/event-list.js
+++ components/results/event-list.js
@@ -5,15 +5,7 @@ const eventList = {
         const vm = this;
         vm.eventData = Service.getJSON();
         vm.events = vm.eventData.data._embedded.events;
-        // console.log(vm.events);
-
-        // events.data._embedded.events //List of events (array of event objects)
-        // [index].dates.start.localDate // Date in format: "yyyy-MM-dd"
-        // [index]._embedded.venues[0].address.line1 // Location Address
-        // [index]._embedded.venues[0].city.name // Location City
-        // [index].name // Event Name
-        // [index]._embedded.venues[""0""].name // Venue Name
-        // [index].url // Link to event in TM
+        console.log(vm.events);
     }]
 }
 
