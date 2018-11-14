6da620b070a89c38379b367be699e7d042cdd3c6
Dakota <dakota.vradenburg@gmail.com>
2018-11-14 14:45:41 -0500

    Styling for event results and favorites

--- components/results/event-list.js
+++ components/results/event-list.js
@@ -5,6 +5,7 @@ const eventList = {
         const vm = this;
         vm.eventData = Service.getJSON();
         vm.events = vm.eventData.data._embedded.events;
+        console.log(vm.events);
         vm.saveFav = (event) => {
             Service.setFav(event);
         };
